f91fc30d1cad65c733833611834b911cb07fc4e8
y.kondo <y.kondo@pepabo.com>
2017-05-23 14:31:36 +0900

    構造を整える

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pagesstatic_pages/home'
+  get  'static_pages/home'
   get  'static_pages/message'
   get  'static_pages/interview'
   get  'static_pages/photo'
