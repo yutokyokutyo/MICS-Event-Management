e3fd25e70ccf021bd9bb13fde0e875eee1d4735e
y.kondo <y.kondo@pepabo.com>
2017-05-25 11:51:04 +0900

    名前付きルート化

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/message'
-  get  'static_pages/interview'
-  get  'static_pages/photo'
-  get  'static_pages/join'
+  get  '/message', to: 'static_pages#message'
+  get  '/interview', to: 'static_pages#interview'
+  get  '/photo', to: 'static_pages#photo'
+  get  '/join', to: 'static_pages#join'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get message" do
-    get static_pages_message_url
+    get message_path
     assert_response :success
   end
 
   test "should get interview" do
-    get static_pages_interview_url
+    get interview_path
     assert_response :success
   end
 
   test "should get photo" do
-    get static_pages_photo_url
+    get photo_path
     assert_response :success
   end
 
   test "should get join" do
-    get static_pages_join_url
+    get join_path
     assert_response :success
   end
 
