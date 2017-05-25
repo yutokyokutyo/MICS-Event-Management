ce84863f28a1bac0d8700e89146ad8a93301847c
y.kondo <y.kondo@pepabo.com>
2017-05-25 12:37:07 +0900

    レイアウトのリンクに対するE2Eテストを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", message_path
+    assert_select "a[href=?]", interview_path
+    assert_select "a[href=?]", photo_path
+    assert_select "a[href=?]", join_path
+  end
 end
