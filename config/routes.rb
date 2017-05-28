23545b4fa1fe923ed764ce9818c7724c542bdee2
y.kondo <k.yuto.cre@gmail.com>
2017-05-28 17:07:38 +0900

    イベント作成用フォームのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/interview', to: 'static_pages#interview'
   get  '/photo', to: 'static_pages#photo'
   get  '/join', to: 'static_pages#join'
+  get  '/event_new',  to: 'events#new'
   resources :events, only: [:new, :create, :edit, :update, :destroy]
 end
