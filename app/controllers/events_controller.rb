e52acf05ede5ee1761b0d27a5cf26655ef644bc8
y.kondo <k.yuto.cre@gmail.com>
2017-05-28 17:55:48 +0900

    イベント用のcreateアクションを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,15 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(params[:id])
+    binding.pry
+    if @event.save
+      flash[:success] = "イベントを作成しました😆"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
 end
