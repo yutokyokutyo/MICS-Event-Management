74c336b09f92b131f6296550b619c50efda2a6a2
y.kondo <k.yuto.cre@gmail.com>
2017-05-28 16:54:33 +0900

    resourceで表現する

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/new'
-
   root 'static_pages#home'
   get  '/message', to: 'static_pages#message'
   get  '/interview', to: 'static_pages#interview'
   get  '/photo', to: 'static_pages#photo'
   get  '/join', to: 'static_pages#join'
-  resources :events, only: [:create, :edit, :update, :destroy]
+  resources :events, only: [:new, :create, :edit, :update, :destroy]
 end
