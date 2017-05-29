34715112e2be2ab97d57265a380421dffa5f2025
y.kondo <y.kondo@pepabo.com>
2017-05-29 12:10:59 +0900

    イベント作成->表示のインターフェイスについてのintegrationテストを追加

--- test/integration/events_interface_test.rb
+++ test/integration/events_interface_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class EventsInterfaceTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "event interface" do
+    get new_event_path
+    assert_select 'input[type="submit"]'
+    title = "Sportsday"
+    sub_title = "justdoit"
+    assert_difference 'Event.count', 1 do
+      post events_path, params: { event: { title: title, sub_title: sub_title } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match title, response.body
+  end
 end
