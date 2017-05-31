cb85f87fc28a13457893abe168fa561d504adc40
y.kondo <y.kondo@pepabo.com>
2017-05-31 12:55:47 +0900

    タイトル以外は必須項目としない

--- db/migrate/20170527082246_create_events.rb
+++ db/migrate/20170527082246_create_events.rb
@@ -2,13 +2,13 @@ class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
       t.text :title,  null: false
-      t.text :sub_title,  null: false
-      t.text :description,  null: false
-      t.string :location,  null: false
-      t.string :image_url,  null: false
-      t.datetime :start_time,  null: false
-      t.datetime :end_time,  null: false
-      t.integer :capacity,  null: false
+      t.text :sub_title
+      t.text :description
+      t.string :location
+      t.string :image_url
+      t.datetime :start_time
+      t.datetime :end_time
+      t.integer :capacity
 
       t.timestamps
     end
