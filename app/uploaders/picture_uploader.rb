a5bfa46b32d2e061ec80ece6c8bced95429508f2
y.kondo <y.kondo@pepabo.com>
2017-06-03 21:46:12 +0900

    横幅は許容できるが縦幅には110pxで制限を設ける

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [110, 110]
+  process resize_to_limit: [200, 110]
   storage :file
 
   # アップロードファイルの保存先ディレクトリは上書き可能
