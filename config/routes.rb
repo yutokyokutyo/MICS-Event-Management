cc6d84d13b1e3b8c81c77afba54decdbf5a8138e
y.kondo <k.yuto.cre@gmail.com>
2017-06-11 13:16:54 +0900

    generateによって作成した不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
