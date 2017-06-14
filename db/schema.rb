b24b5c118b40543053b8b67eaa61bff67cb306e5
y.kondo <y.kondo@pepabo.com>
2017-06-14 17:31:11 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611043758) do
+ActiveRecord::Schema.define(version: 20170614024006) do
 
   create_table "events", force: :cascade do |t|
     t.text     "title"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20170611043758) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "picture"
+    t.integer  "user_id"
+    t.index ["user_id", "created_at"], name: "index_events_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
