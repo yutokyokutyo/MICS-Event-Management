9167b2a52289654d165ea96df7e624423f520380
y.kondo <k.yuto.cre@gmail.com>
2017-06-17 23:17:43 +0900

    joined_idがあった初めてリレーションシップなんで

--- db/migrate/20170616021946_create_relationships.rb
+++ db/migrate/20170616021946_create_relationships.rb
@@ -2,8 +2,12 @@ class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :relationships do |t|
       t.integer :join_id
+      t.integer :joined_id
+
       t.timestamps
     end
     add_index :relationships, :join_id
+    add_index :relationships, :joined_id
+    add_index :relationships, [:join_id, :joined_id], unique: true
   end
 end
