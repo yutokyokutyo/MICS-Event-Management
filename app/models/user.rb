2764e162f7beb3edc0097ea718b19511dc0db257
y.kondo <k.yuto.cre@gmail.com>
2017-06-17 13:03:41 +0900

    Userモデルにjoinの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "join_id",
                                   dependent:   :destroy
+  has_many :join, through: :active_relationships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
