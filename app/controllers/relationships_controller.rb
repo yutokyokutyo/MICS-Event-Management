f5273c610d548ee350cfb3d0c01881be262d8375
y.kondo <k.yuto.cre@gmail.com>
2017-06-17 15:30:51 +0900

    Relationshipsコントローラ

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
+    event = Event.find(params[:joined_id])
+    current_user.join(event)
+    redirect_to event
   end
 
   def destroy
+    event = Relationship.find(params[:id]).joined
+    current_user.unjoin(event)
+    redirect_to event
   end
 end
