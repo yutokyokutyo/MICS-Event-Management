9ef878cec6ea1524aa67ad6a172efa62865289f6
y.kondo <k.yuto.cre@gmail.com>
2017-06-17 13:10:22 +0900

    join関連メソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # イベントに参加する
+  def join(event)
+    active_relationships.create(joined_id: event.id)
+  end
+
+  # イベント参加キャンセルする
+  def unjoin(event)
+    active_relationships.find_by(joined_id: event.id).destroy
+  end
+
+  # 現在のユーザーがイベント参加していたらtrueを返す
+  def join?(event)
+    following.include?(event)
+  end
 end
