ac8a37f3afd1390c5f417a7ff317936262989a78
y.kondo <k.yuto.cre@gmail.com>
2017-06-18 16:37:15 +0900

    ユーザーが参加しているイベント情報を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = @user.events.paginate(page: params[:page])
+    @joining_events = @user.joining.paginate(page: params[:page])
   end
 end
