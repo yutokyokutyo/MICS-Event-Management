4b52cf89b09b628d32213a4bafd8520ed899356e
y.kondo <y.kondo@pepabo.com>
2017-06-19 12:25:34 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618225515) do
+ActiveRecord::Schema.define(version: 20170619032243) do
 
   create_table "events", force: :cascade do |t|
     t.text     "title",       null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20170618225515) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
