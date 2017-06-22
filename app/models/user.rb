b8f53079299152b159f64ba86a5025dff5adb317
y.kondo <y.kondo@pepabo.com>
2017-06-22 10:55:28 +0900

    PictureUploaderクラスを参照できるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  mount_uploader :image, PictureUploader
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
