620d62e5d476a0adbb61fd0eb756831868672017
y.kondo <y.kondo@pepabo.com>
2017-06-22 10:54:26 +0900

    strong parameterにimageを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # strong parameter
   def configure_permitted_parameters
-    added_attrs = [ :name, :email, :password, :password_confirmation　]
+    added_attrs = [ :name, :email, :image, :password, :password_confirmation　]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
