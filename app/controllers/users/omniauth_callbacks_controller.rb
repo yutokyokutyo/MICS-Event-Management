31791b201d6c0812b9a53a5f1a1b0af45e791e1a
y.kondo <y.kondo@pepabo.com>
2017-06-23 09:06:43 +0900

    callback処理を汎用的にするためメソッドに切り出す

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    callback_from :facebook
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def callback_from(provider)
+    provider =  provider.to_s
+
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
@@ -7,12 +19,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
 
-  def failure
-    redirect_to root_path
-  end
 end
