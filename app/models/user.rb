f27356d937a3b7e597c723d6c7f664c7ad8f0029
y.kondo <y.kondo@pepabo.com>
2017-06-23 10:36:53 +0900

    emailにダミーアドレスを入れる
    TODO: emailが取得できなかった場合はダミーアドレスが入るようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = User.dummy_email(auth)
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
@@ -37,4 +37,10 @@ class User < ApplicationRecord
   def joining?(event)
     joining.include?(event)
   end
+
+  private
+
+    def self.dummy_email(auth)
+      "#{auth.uid}-#{auth.provider}@example.com"
+    end
 end
